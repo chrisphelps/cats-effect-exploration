9dc0acbaffec71d96381b4da52d947fbc644984f
Chris Phelps <cjphelp@gmail.com>
2023-03-01 16:18:51 -0700

    Initial import, cats-effect copy file tutorial

---
+++ src/main/scala/com/sutemi/CopyFile.scala
@@ -0,0 +1,47 @@
+package com.sutemi
+
+import cats.effect.IO
+import cats.effect.Resource
+
+import java.io.{File, FileInputStream, FileOutputStream, InputStream, OutputStream}
+
+object CopyFile {
+
+  def copy(origin: File, destination: File): IO[Long] =
+    inputOutputStreams(origin, destination).use { case(in, out) =>
+      transfer(in, out)
+    }
+
+  def transfer(origin: InputStream, destination: OutputStream): IO[Long] =
+    transmit(origin, destination, new Array[Byte](1024 * 10), 0L)
+
+  def transmit(origin: InputStream, destination: OutputStream, buffer: Array[Byte], acc: Long): IO[Long] =
+    for {
+      amount <- IO.blocking(origin.read(buffer, 0, buffer.size))
+      count <- if (amount > -1) IO.blocking(destination.write(buffer, 0, amount)) >> transmit(origin, destination, buffer, acc + amount)
+        else IO.pure(acc)
+    } yield count
+
+  def inputStream(f: File): Resource[IO, FileInputStream] =
+    Resource.make {
+      IO.blocking(new FileInputStream(f))
+    } { inStream =>
+      IO.blocking(inStream.close()).handleErrorWith(_ => IO.unit)
+      // this error handler swallows the errors
+    }
+
+  def outputStream(f: File): Resource[IO, FileOutputStream] =
+    Resource.make {
+      IO.blocking(new FileOutputStream(f))
+    } { outStream =>
+      IO.blocking(outStream.close()).handleErrorWith(_ => IO.unit)
+    }
+
+  def inputOutputStreams(in: File, out: File): Resource[IO, (FileInputStream, FileOutputStream)] =
+    for {
+      inStream <- inputStream(in)
+      outStream <- outputStream(out)
+    } yield (inStream, outStream)
+
+
+}
---
+++ src/main/scala/com/sutemi/Main.scala
@@ -0,0 +1,22 @@
+package com.sutemi
+
+import cats.effect.{ExitCode, IO, IOApp}
+
+import java.io.File
+
+object Main extends IOApp {
+
+  // This is your new "main"!
+//  def run: IO[Unit] =
+//    HelloWorld.say().flatMap(IO.println)
+
+  def run(args: List[String]): IO[ExitCode] =
+    for {
+      _ <- if (args.length < 2) IO.raiseError(new IllegalArgumentException("Need origin and destination files"))
+        else IO.unit
+      orig = new File(args(0))
+      dest = new File(args(1))
+      count <- CopyFile.copy(orig, dest)
+      _ <- IO.println(s"Copied $count bytes from ${orig.getPath} to ${dest.getPath}")
+    } yield ExitCode.Success
+}
